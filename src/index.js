73cd1f67b0587c02890947aadd3e9a4867aa1dbe
Caleb Smith <calebsmith@Calebs-MacBook-Pro-2.local>
2021-05-06 09:07:07 -0600

    removed hashrouter

--- src/index.js
+++ src/index.js
@@ -5,14 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
